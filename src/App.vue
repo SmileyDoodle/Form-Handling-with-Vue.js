6e40f834077227b5dbf677947e42b1a10e8c16a6
SmileyDoodle <smileydoodle.official@gmail.com>
2020-10-29 14:44:31 +0200

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Form></Form>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Form from './components/Form.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Form
   }
 }
 </script>
 
 <style>
+body {
+  background: rgb(204,204,204); 
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
---
+++ src/components/Form.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="A4"></div>
+</template>
+
+<script>
+export default {
+    name: 'Form'
+}
+</script>
+
+<style>
+.A4 {
+  width: 21cm;
+  height: 29.7cm; 
+  background: white;
+  display: block;
+  margin: 0 auto;
+  margin-bottom: 0.5cm;
+  box-shadow: 0 0 0.5cm rgba(0,0,0,0.5);
+}
+</style>
